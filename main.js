5c0eadb15f431dd904b71cf92b14746d467bf5eb
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-04-18 23:00:07 +0300

    hover listelement update

--- main.js
+++ main.js
@@ -19,11 +19,14 @@ $adressButton3.on('click', () => {$Address3.slideToggle(), $Address2.slideUp(), 
 
 $adressButton4.on('click', () => {$Address4.slideToggle(), $Address3.slideUp(), $Address2.slideUp(), $Address1.slideUp()
 })
-$adressButton1.on('click', () =>{ $adressButton1.css('background-color', '#fff')})
-$adressButton2.on('click', () =>{ $adressButton2.css('background-color', '#fff')})
-$adressButton3.on('click', () =>{ $adressButton3.css('background-color', '#fff')})
-$adressButton4.on('click', () =>{ $adressButton4.css('background-color', '#fff')})
+$adressButton1.on('click', () =>{ $adressButton1.css('background-color', '#fff'), $adressButton2.css('background-color', '#f2f5f6'), $adressButton3.css('background-color', '#f2f5f6'), $adressButton4.css('background-color', '#f2f5f6')})
+$adressButton2.on('click', () =>{ $adressButton2.css('background-color', '#fff'), $adressButton3.css('background-color', '#f2f5f6'), $adressButton4.css('background-color', '#f2f5f6'), $adressButton1.css('background-color', '#f2f5f6')})
+$adressButton3.on('click', () =>{ $adressButton3.css('background-color', '#fff'), $adressButton2.css('background-color', '#f2f5f6'), $adressButton4.css('background-color', '#f2f5f6'), $adressButton1.css('background-color', '#f2f5f6')})
+$adressButton4.on('click', () =>{ $adressButton4.css('background-color', '#fff'), $adressButton3.css('background-color', '#f2f5f6'), $adressButton2.css('background-color', '#f2f5f6'), $adressButton1.css('background-color', '#f2f5f6')})
 
-
+$adressButton1.on('click', () =>{ $adressButton1.css('border-left', '4px solid #293135'), $adressButton3.css('border-left', '4px solid #f2f5f6'), $adressButton2.css('border-left', '4px solid #f2f5f6'), $adressButton4.css('border-left', '4px solid #f2f5f6')})
+$adressButton2.on('click', () =>{ $adressButton2.css('border-left', '4px solid #293135'), $adressButton3.css('border-left', '4px solid #f2f5f6'), $adressButton4.css('border-left', '4px solid #f2f5f6'), $adressButton1.css('border-left', '4px solid #f2f5f6')})
+$adressButton3.on('click', () =>{ $adressButton3.css('border-left', '4px solid #293135'), $adressButton4.css('border-left', '4px solid #f2f5f6'), $adressButton2.css('border-left', '4px solid #f2f5f6'), $adressButton1.css('border-left', '4px solid #f2f5f6')})
+$adressButton4.on('click', () =>{ $adressButton4.css('border-left', '4px solid #293135'), $adressButton3.css('border-left', '4px solid #f2f5f6'), $adressButton2.css('border-left', '4px solid #f2f5f6'), $adressButton1.css('border-left', '4px solid #f2f5f6')})
 
 });
